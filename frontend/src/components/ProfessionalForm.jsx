79e52a3b1b102aba92aa102e2d9f21535b8cd7cc
Wius <wius93@gmail.com>
2024-02-17 20:08:09 -0300

    Agregar JS a ProfessionalForm

--- frontend/src/components/ProfessionalForm.jsx
+++ frontend/src/components/ProfessionalForm.jsx
@@ -1,27 +1,55 @@
+import React, { useState } from 'react';
+import axios from 'axios';
 import '../stylesheets/ProfessionalForm.css'
 
 const ProfessionalForm = () => {
+  const [formData, setFormData] = useState({
+    first_name: '',
+    last_name: '',
+    phone_number: '',
+    email: '',
+    usuario: '',
+    password: '',
+    description: '',
+    speciality: '',
+    barrio: '',
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post('https://render-api-a6du.onrender.com/', formData);
+      console.log('Response:', response.data);
+    } catch (error) {
+      console.error('Error:', error.response.data);
+    }
+  };
 
   return (
     <div>
       <h2>Registrate como<br />
         <span>profesional</span></h2>
+      <form onSubmit={handleSubmit}>
       <div className="info-1">
         <h3>Información personal</h3>
         <div>
           <div className="fila">
-            <input className="input-box" type="text" placeholder="Nombre" required name="Nombre" />
-            <input type="text" placeholder="Apellido" required name="Apellido" />
+            <input className="input-box" type="text" placeholder="Nombre" required name="Nombre" value={formData.first_name} onChange={handleChange} />
+            <input type="text" placeholder="Apellido" required name="Apellido" value={formData.last_name} onChange={handleChange} />
           </div>
           <div className="fila">
-            <input type="tel" placeholder="Teléfono" required name="Telefono" />
-            <input type="email" name="email" id="email" required placeholder="Email" />
+            <input type="tel" placeholder="Teléfono" required name="Telefono" value={formData.phone_number} onChange={handleChange} />
+            <input type="email" name="email" id="email" required placeholder="Email" value={formData.email} onChange={handleChange} />
           </div>
           <div>
-            <input type="text" name="usuario" id="usuario" placeholder="Usuario" required />
+            <input type="text" name="usuario" id="usuario" placeholder="Usuario" required value={formData.username} onChange={handleChange} />
           </div>
           <div className="fila">
-            <input type="password" name="contraseña" placeholder="Contraseña" required />
+            <input type="password" name="contraseña" placeholder="Contraseña" required value={formData.password} onChange={handleChange} />
             <input type="password" name="contraseña-verificar" placeholder="Verifique su contraseña" required />
           </div>
         </div>
@@ -29,7 +57,7 @@ const ProfessionalForm = () => {
       <div className="info-2">
         <h3>Informacion profesional</h3>
         <div>
-          <select name="discipline" id="discipline">
+          <select name="discipline" id="discipline"  value={formData.speciality} onChange={handleChange}>
             <option value="Boxeo">Boxeo</option>
             <option value="Zumba">Zumba</option>
             <option value="Crossfit">Crossfit</option>
@@ -37,53 +65,53 @@ const ProfessionalForm = () => {
             <option value="Pilates">Pilates</option>
             <option value="Yoga">Yoga</option>
           </select>
-          <textarea name="descripcion" id="descripcion" placeholder="Descripción" readonly></textarea>
+          <textarea name="descripcion" id="descripcion" placeholder="Descripción" readonly value={formData.description} onChange={handleChange}></textarea>
         </div>
       </div>
       <div className="info-3">
         <h3>Información clases</h3>
         <div>
-          <input type="text" name="Barrio" id="Barrio" required placeholder="Barrio" />
+          <input type="text" name="Barrio" id="Barrio" required placeholder="Barrio" value={formData.barrio} onChange={handleChange} />
         </div>
         <div id="contenedor">
           <div>
             <div className="posicion">
               <div className="opciones-checkbox">
-                <input type="checkbox" name="lunes-opcion" id="lunes-opcion" />
+                <input type="checkbox" name="lunes-opcion" id="lunes-opcion" value={formData['lunes-opcion']} onChange={handleChange} />
                 <label for="lunes-opcion">Lunes</label>
               </div>
               <div className="columna-de-tiempo">
                 <div>
-                  <input type="time" name="opcion1-lunes" />
-                  <input type="time" name="opcion1-2-lunes" />
+                  <input type="time" name="opcion1-lunes" value={formData['opcion1-lunes']} onChange={handleChange} />
+                  <input type="time" name="opcion1-2-lunes" value={formData['opcion1-2-lunes']} onChange={handleChange} />
                 </div>
                 <div>
-                  <input type="time" name="opcion2-lunes" />
-                  <input type="time" name="opcion2-2-lunes" />
+                  <input type="time" name="opcion2-lunes" value={formData['opcion2-lunes']} onChange={handleChange} />
+                  <input type="time" name="opcion2-2-lunes" value={formData['opcion2-2-lunes']} onChange={handleChange} />
                 </div>
                 <div>
-                  <input type="time" name="opcion3-lunes" />
-                  <input type="time" name="opcion3-2-lunes" />
+                  <input type="time" name="opcion3-lunes" value={formData['opcion3-lunes']} onChange={handleChange} />
+                  <input type="time" name="opcion3-2-lunes" value={formData['opcion3-2-lunes']} onChange={handleChange} />
                 </div>
               </div>
             </div>
             <div className="posicion">
               <div className="opciones-checkbox">
-                <input type="checkbox" name="martes-opcion" id="martes-opcion" />
+                <input type="checkbox" name="martes-opcion" id="martes-opcion" value={formData['martes-opcion']} onChange={handleChange} />
                 <label for="martes-opcion">Martes</label>
               </div>
               <div className="columna-de-tiempo">
                 <div>
-                  <input type="time" name="opcion1-martes" />
-                  <input type="time" name="opcion1-2-martes" />
+                  <input type="time" name="opcion1-martes" value={formData['opcion1-martes']} onChange={handleChange} />
+                  <input type="time" name="opcion1-2-martes" value={formData['opcion1-2-martes']} onChange={handleChange} />
                 </div>
                 <div>
-                  <input type="time" name="opcion2-martes" />
-                  <input type="time" name="opcion2-2-martes" />
+                  <input type="time" name="opcion2-martes" value={formData['opcion2-martes']} onChange={handleChange} />
+                  <input type="time" name="opcion2-2-martes" value={formData['opcion2-2-martes']} onChange={handleChange} />
                 </div>
                 <div>
-                  <input type="time" name="opcion3-martes" />
-                  <input type="time" name="opcion3-2-martes" />
+                  <input type="time" name="opcion3-martes" value={formData['opcion3-martes']} onChange={handleChange} />
+                  <input type="time" name="opcion3-2-martes" value={formData['opcion3-2-martes']} onChange={handleChange} />
                 </div>
               </div>
             </div>
@@ -91,41 +119,41 @@ const ProfessionalForm = () => {
           <div>
             <div className="posicion">
               <div className="opciones-checkbox">
-                <input type="checkbox" name="miercoles-opcion" id="miercoles-opcion" />
+                <input type="checkbox" name="miercoles-opcion" id="miercoles-opcion" value={formData['miercoles-opcion']} onChange={handleChange} />
                 <label for="miercoles-opcion">Miércoles</label>
               </div>
               <div className="columna-de-tiempo">
                 <div>
-                  <input type="time" name="opcion1-miercoles" />
-                  <input type="time" name="opcion1-2-miercoles" />
+                  <input type="time" name="opcion1-miercoles" value={formData['opcion1-miercoles']} onChange={handleChange} />
+                  <input type="time" name="opcion1-2-miercoles" value={formData['opcion1-2-miercoles']} onChange={handleChange} />
                 </div>
                 <div>
-                  <input type="time" name="opcion2-miercoles" />
-                  <input type="time" name="opcion2-2-miercoles" />
+                  <input type="time" name="opcion2-miercoles" value={formData['opcion2-miercoles']} onChange={handleChange} />
+                  <input type="time" name="opcion2-2-miercoles" value={formData['opcion2-2-miercoles']} onChange={handleChange} />
                 </div>
                 <div>
-                  <input type="time" name="opcion3-miercoles" />
-                  <input type="time" name="opcion3-2-miercoles" />
+                  <input type="time" name="opcion3-miercoles" value={formData['opcion3-miercoles']} onChange={handleChange} />
+                  <input type="time" name="opcion3-2-miercoles" value={formData['opcion3-2-miercoles']} onChange={handleChange} />
                 </div>
               </div>
             </div>
             <div className="posicion">
               <div className="opciones-checkbox">
-                <input type="checkbox" name="jueves-opcion" id="jueves-opcion" />
+                <input type="checkbox" name="jueves-opcion" id="jueves-opcion" value={formData['jueves-opcion']} onChange={handleChange} />
                 <label for="jueves-opcion">Jueves</label>
               </div>
               <div className="columna-de-tiempo">
                 <div>
-                  <input type="time" name="opcion1-jueves" />
-                  <input type="time" name="opcion1-2-jueves" />
+                  <input type="time" name="opcion1-jueves" value={formData['opcion1-jueves']} onChange={handleChange} />
+                  <input type="time" name="opcion1-2-jueves" value={formData['opcion1-2-jueves']} onChange={handleChange} />
                 </div>
                 <div>
-                  <input type="time" name="opcion2-jueves" />
-                  <input type="time" name="opcion2-2-jueves" />
+                  <input type="time" name="opcion2-jueves" value={formData['opcion2-jueves']} onChange={handleChange} />
+                  <input type="time" name="opcion2-2-jueves" value={formData['opcion2-2-jueves']} onChange={handleChange} />
                 </div>
                 <div>
-                  <input type="time" name="opcion3-jueves" />
-                  <input type="time" name="opcion3-2-jueves" />
+                  <input type="time" name="opcion3-jueves" value={formData['opcion3-jueves']} onChange={handleChange} />
+                  <input type="time" name="opcion3-2-jueves" value={formData['opcion3-2-jueves']} onChange={handleChange} />
                 </div>
               </div>
             </div>
@@ -133,41 +161,41 @@ const ProfessionalForm = () => {
           <div>
             <div className="posicion">
               <div className="opciones-checkbox">
-                <input type="checkbox" name="viernes-opcion" id="viernes-opcion" />
+                <input type="checkbox" name="viernes-opcion" id="viernes-opcion" value={formData['viernes-opcion']} onChange={handleChange} />
                 <label for="viernes-opcion">Viernes</label>
               </div>
               <div className="columna-de-tiempo">
                 <div>
-                  <input type="time" name="opcion1-viernes" />
-                  <input type="time" name="opcion1-2-viernes" />
+                  <input type="time" name="opcion1-viernes" value={formData['opcion1-viernes']} onChange={handleChange} />
+                  <input type="time" name="opcion1-2-viernes" value={formData['opcion1-2-viernes']} onChange={handleChange} />
                 </div>
                 <div>
-                  <input type="time" name="opcion2-viernes" />
-                  <input type="time" name="opcion2-2-viernes" />
+                  <input type="time" name="opcion2-viernes" value={formData['opcion2-viernes']} onChange={handleChange} />
+                  <input type="time" name="opcion2-2-viernes" value={formData['opcion2-2-viernes']} onChange={handleChange} />
                 </div>
                 <div>
-                  <input type="time" name="opcion3-viernes" />
-                  <input type="time" name="opcion3-2-viernes" />
+                  <input type="time" name="opcion3-viernes" value={formData['opcion3-viernes']} onChange={handleChange} />
+                  <input type="time" name="opcion3-2-viernes" value={formData['opcion3-2-viernes']} onChange={handleChange} />
                 </div>
               </div>
             </div>
             <div className="posicion">
               <div className="opciones-checkbox">
-                <input type="checkbox" name="sabado-opcion" id="sabado-opcion" />
+                <input type="checkbox" name="sabado-opcion" id="sabado-opcion" value={formData['sabado-opcion']} onChange={handleChange} />
                 <label for="sabado-opcion">Sabado</label>
               </div>
               <div className="columna-de-tiempo">
                 <div>
-                  <input type="time" name="opcion1-sabado" />
-                  <input type="time" name="opcion1-2-sabado" />
+                  <input type="time" name="opcion1-sabado" value={formData['opcion1-sabado']} onChange={handleChange} />
+                  <input type="time" name="opcion1-2-sabado" value={formData['opcion1-2-sabado']} onChange={handleChange} />
                 </div>
                 <div>
-                  <input type="time" name="opcion2-sabado" />
-                  <input type="time" name="opcion2-2-sabado" />
+                  <input type="time" name="opcion2-sabado" value={formData['opcion2-sabado']} onChange={handleChange} />
+                  <input type="time" name="opcion2-2-sabado" value={formData['opcion2-2-sabado']} onChange={handleChange} />
                 </div>
                 <div>
-                  <input type="time" name="opcion3-sabado" />
-                  <input type="time" name="opcion3-2-sabado" />
+                  <input type="time" name="opcion3-sabado" value={formData['opcion3-sabado']} onChange={handleChange} />
+                  <input type="time" name="opcion3-2-sabado" value={formData['opcion3-2-sabado']} onChange={handleChange} />
                 </div>
               </div>
             </div>
@@ -177,10 +205,9 @@ const ProfessionalForm = () => {
       <div id="contenedor-enviar">
         <input type="submit" name="registro" value="Registrarte" id="enviar" />
       </div>
-
+      </form>
     </div>
   )
 }
 
 export default ProfessionalForm
-
