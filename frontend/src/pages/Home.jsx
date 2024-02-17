7058e278d541af04e241ef0cf95bdc6f28b78233
Wius <wius93@gmail.com>
2024-02-17 12:21:53 -0300

    Cambios menores

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Header from '../components/Header.jsx'
 import Footer from '../components/Footer.jsx'
+import FormContainer from '../components/FormContainer.jsx'
 
 
 const Home = () => {
   return (
     <div>
       <Header />
-
+        <FormContainer />
       <Footer />
     </div>
   )
