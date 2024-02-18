acd11bc944ee3df68834c5ea0d55d10305314cc2
Martin Roige <martinroige@hotmail.com>
2024-02-18 19:39:08 -0300

    Un poco mas completo la vista de usuario

--- frontend/src/components/Novedades.jsx
+++ frontend/src/components/Novedades.jsx
@@ -5,6 +5,32 @@ const Novedades = () => {
   return (
     <div class="novedades">
        <h1 class="titulo">Proximos Turnos</h1>
+       <div class="novedadBox">
+        <p>Disciplina</p>
+        <h3>Profesional</h3>
+        <div class="novedadBoxFecha">
+          <p>Dia de la semana + n </p>
+          <p>00:00 - 00:00 </p>
+        </div>
+       </div> 
+
+       <div class="novedadBox">
+        <p>Disciplina</p>
+        <h3>Profesional</h3>
+        <div class="novedadBoxFecha">
+          <p>Dia de la semana + n </p>
+          <p>00:00 - 00:00 </p>
+        </div>
+       </div> 
+
+       <div class="novedadBox">
+        <p>Disciplina</p>
+        <h3>Profesional</h3>
+        <div class="novedadBoxFecha">
+          <p>Dia de la semana + n </p>
+          <p>00:00 - 00:00 </p>
+        </div>
+       </div> 
       
     </div>
   )
