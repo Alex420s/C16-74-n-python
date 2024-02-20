7b43595f20f12cccd1e5ead933c92fa9f08c1756
Wius <wius93@gmail.com>
2024-02-20 09:46:35 -0300

    Cambios menores

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import { React, useEffect, useState } from 'react'
 import logo from '../images/Logo.png'
 import '../stylesheets/Header.css'
 import { Link } from 'react-router-dom'
@@ -10,11 +10,30 @@ const Header = () => {
     console.log("logged out");
   }
 
+  // const [IsLoggedIn, setIsLoggedIn] = useState(false);
+
+  // useEffect(() => {
+  //   try {
+  //     const storedUser = localStorage.getItem('token');
+  //     console.log(localStorage.getItem('token'));
+  //     if (storedUser) {
+  //       setIsLoggedIn(true);
+  //     }
+  //   } catch (error) {
+  //     console.error('Error retrieving user from storage:', error);
+  //   }
+  // }, []);
+
   return (
     <div>
       <header className='navbar'>
-        <Link to={'/'}><img className="header-logo" src={logo} alt="Logo"/></Link>
+        <Link to={'/'}><img className="header-logo" src={logo} alt="Logo" /></Link>
         <ul>
+          {/* {IsLoggedIn ? (
+            <li className='rojo hover'><Link to={'/'} className='nav-link' onClick={logout}>Logout</Link></li>
+          ) : (
+            <li className='rojo hover'><Link to={'/ingresar'} className='nav-link'>Ingresar</Link></li>
+          )} */}
           <li className='rojo hover'><Link to={'/ingresar'} className='nav-link'>Ingresar</Link></li>
           <li className='rojo hover'><Link to={'/'} className='nav-link' onClick={logout}>Logout</Link></li>
         </ul>
