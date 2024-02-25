010826c50be121384f23ccdf805389cb084dd7d9
Verónica <molinariveronica478@gmail.com>
2024-02-24 22:11:09 -0300

    Arreglando la logica del search

--- frontend/src/components/SearchPage.jsx
+++ frontend/src/components/SearchPage.jsx
@@ -1,64 +1,51 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios';
+import React, { useState } from 'react'
 import '../stylesheets/Search.css'
 import flechaIzq from '../images/flechaIzquierda.png';
 import flechaDer from '../images/flechaDerecha.png';
+
+const objetoPrueba = [
+    {
+        nombre: "John",
+        apellido: "Doe",
+        especialidad: "Boxeo",
+        descripcion: "Lorem ipsum dolor sit amet",
+        barrio: "Example",
+        provincia: "Example",
+        valorSesion: "$ 2500",
+        disponibilidad: [["13:00 - 14:00"],["15:00 - 16:00"],["14:00 - 15:00"],["10:00 - 11:00"],["18:00 - 19:00"],["10:00 - 11:00"]],
+    },
+    {
+        nombre: "Juan",
+        apellido: "Perez",
+        especialidad: "Zumba",
+        descripcion: "Lorem ipsum dolor sit amet",
+        barrio: "Villa Urquiza",
+        provincia: "Villa Urquiza",
+        valorSesion: "$ 5000",
+        disponibilidad: [["15:00 - 16:00"], [""],["18:00 - 19:00"], ["10:00 - 11:00"],[""],[""]],
+    },
+    {
+        nombre: "Daniel",
+        apellido: "Garcia",
+        especialidad: "Spinning",
+        descripcion: "Lorem ipsum dolor sit amet",
+        barrio: "Boedo",
+        provincia: "Boedo",
+        valorSesion: "$ 3500",
+        disponibilidad: [["15:00 - 16:00"], [""], ["18:00 - 19:00"], ["10:00 - 11:00"],[""],[""]],
+    }
+]
 
 const SearchPage = () => {
     const hoy = new Date(); // Fecha actual
     const [mostrarCalendario, setMostrarCalendario] = useState(false);
     const [fechaActual, setFechaActual] = useState(hoy);
-    const [celdasSeleccionadas, setCeldasSeleccionadas] = useState(new Set());
     const [posicionCalendario, setPosicionCalendario] = useState(0); 
-  
-    const handleVer = (id) => {
-        // Aquí puedes agregar lógica para determinar la posición de la fila específica con respecto a la parte superior de la ventana del navegador
-    
-        // Esto es para abrir el calendario debajo del botón "Ver" en la fila específica
-        const fila = document.getElementById(id);
-        const posicionFila = fila.offsetTop + fila.offsetHeight;
-    
-        setPosicionCalendario(posicionFila); // Actualizamos el estado con la posición de apertura del calendario
-        setMostrarCalendario(true);
-    };
+    const [idPersonaSeleccionada, setIdPersonaSeleccionada] = useState(null);
 
-    const handleSeleccionarHorario = (hora) => {
-        
-    };
-        
-    const handleReservar = () => {
-        /* Lógica para reservar el horario seleccionado */
-    };
-    
-    const CalendarioHorarios = ({ diasSemana, obtenerFecha }) => {
-        // Horarios de ejemplo para cada día
-        const horarios = [
-            ["13:00 - 14:00", "", "13:00 - 14:00", "13:00 - 14:00", "", ""],
-            ["13:00 - 14:00", "", "13:00 - 14:00", "13:00 - 14:00", "", ""]
-        ];
-    
-        return (
-            <table className="calendario-table">
-                <thead>
-                    <tr>
-                        {diasSemana.map((dia, indice) => (
-                            <th key={indice}>{dia} <br /> {obtenerFecha(indice + 1)}</th>
-                        ))}
-                    </tr>
-                </thead>
-                <tbody>
-                    {horarios.map((fila, indexFila) => (
-                        <tr key={indexFila}>
-                            {fila.map((hora, indexColumna) => (
-                                <td key={indexColumna} data-hora={hora} onClick={() => handleSeleccionarHorario(hora)}>
-                                    {hora}
-                                </td>
-                            ))}
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-        );
+    const handleVer = (id) => {
+      setIdPersonaSeleccionada(id);
+      setMostrarCalendario(true);
     };
 
     const handleAnteriorSemana = () => {
@@ -73,21 +60,53 @@ const SearchPage = () => {
         setFechaActual(nuevaFecha);
     };
 
-    const EncabezadoCalendario = ({fecha}) => {
-        function obtenerFecha(dia) {
-            const diaActual = fecha.getDay();
-            const diferenciaDias = dia - diaActual;
-            const nuevaFecha = new Date(fecha.getTime() + (diferenciaDias * 24 * 60 * 60 * 1000));
-            const diaNumero = nuevaFecha.getDate();
-            const mes = nuevaFecha.getMonth() + 1;
-            return `${diaNumero < 10 ? '0' : ''}${diaNumero}/${mes < 10 ? '0' : ''}${mes}`;
-        }
-    
-        return (
-            <CalendarioHorarios diasSemana={["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"]} obtenerFecha={obtenerFecha} />
-        );
+    const handleReservar = () => {
+        console.log(idPersonaSeleccionada);
     }
 
+    const obtenerFecha = (fecha, dia) => {
+        if (!(fecha instanceof Date)) {
+        fecha = new Date(); // Si no es una fecha válida, utilizamos la fecha actual
+    }
+        const diaActual = fecha.getDay();
+        const diferenciaDias = dia - diaActual;
+        const nuevaFecha = new Date(fecha.getTime() + (diferenciaDias * 24 * 60 * 60 * 1000));
+        const diaNumero = nuevaFecha.getDate();
+        const mes = nuevaFecha.getMonth() + 1;
+        return `${diaNumero < 10 ? '0' : ''}${diaNumero}/${mes < 10 ? '0' : ''}${mes}`;
+    };
+    const Calendario = () => {
+        const EncabezadoCalendario = ({fecha}) => {
+                   
+        }
+          
+        return (
+          <div className="calendario">
+            <EncabezadoCalendario fecha={fechaActual}/>
+            <div className="tabla-calendario">
+              {["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"].map((dia, indice) => (
+                <th key={indice}>{dia} <br /> {obtenerFecha(indice + 1)}</th>
+              ))}
+              <tbody>
+                {[...Array(6)].map((_, indexFila) => (
+                  <tr key={indexFila}>
+                    {objetoPrueba[idPersonaSeleccionada].disponibilidad.map((horarios, indexColumna) => (
+                        <td key={indexColumna} data-horario={horarios[indexFila]}>
+                            {horarios[indexFila]}
+                        </td>
+                    ))}
+                  </tr>
+                ))}
+              </tbody>
+            </div>
+            <div className="navegacion-calendario">
+              <button className='mover' onClick={handleAnteriorSemana}><img src={flechaIzq} alt="" /> </button>
+              <button className='mover' onClick={handleSiguienteSemana}><img src={flechaDer} alt="" /> </button>
+            </div>
+          </div>
+        );
+      };
+    
     return (
       <div className="listado">
         <h1 className="texto_rojo">Resultados búsqueda</h1>
@@ -105,41 +124,27 @@ const SearchPage = () => {
                 </tr>
             </thead>
             <tbody>
-                {/*falta la lógica del back*/}
-                <tr>
-                    <td>Prueba</td>
-                    <td>Uno</td>
-                    <td>Boxeo</td>
-                    <td>Las mejores clases</td>
-                    <td>Villa Urquiza</td>
-                    <td>CABA - Bs As</td>
-                    <td>$ 2000</td>
-                    <td><input className='ver hover' type="submit" name="ver" value="Ver" onClick={() => handleVer('idDeLaFila')} /></td>          
+                {objetoPrueba && objetoPrueba.map((persona, index) => (
+                <tr key={index} id={`persona-${index}`}>
+                    <td>{persona.nombre}</td>
+                    <td>{persona.apellido}</td>
+                    <td>{persona.especialidad}</td>
+                    <td>{persona.descripcion}</td>
+                    <td>{persona.barrio}</td>
+                    <td>{persona.provincia}</td>
+                    <td>{persona.valorSesion}</td>
+                    <td><button onClick={() => handleVer(index)}>Ver</button></td>
                 </tr>
-                {/*en el handlever podemos usar el id del profesional desde la tabla*/}
-                <tr>
-                    <td>Prueba</td>
-                    <td>Dos</td>
-                    <td>Zumba</td>
-                    <td>Me aburre inventar</td>
-                    <td>Villa Pueyrredón</td>
-                    <td>CABA - Bs As</td>
-                    <td>$ 2000</td>
-                    <td><input className='ver hover' type="submit" name="ver" value="Ver" onClick={() => handleVer('idDeLaFila')} /></td>          
-                </tr>
+                ))}
             </tbody>
         </table>
         {mostrarCalendario && (
-          <div className="calendario">
-            <EncabezadoCalendario fecha={fechaActual}/>
-            <div>
-                <button className='mover' onClick={handleAnteriorSemana}><img src={flechaIzq} alt="" /> </button>
-                <button className='mover' onClick={handleSiguienteSemana}><img src={flechaDer} alt="" /> </button>
-            </div>    
-            <div className="posicion2">  
-                <button className="reserva" onClick={handleReservar}>Reservar</button>
-            </div>
+          <><Calendario/>
+          <div className="posicion2">  
+              <button className="reserva" onClick={handleReservar}>Reservar</button>
           </div>
+          </>
+        
         )}
         
         </div>
