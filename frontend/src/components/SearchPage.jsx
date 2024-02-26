ef6e47a448f8b82c04038ce10ea4cd8a3c6610e7
Verónica <molinariveronica478@gmail.com>
2024-02-26 09:19:04 -0300

    Modificando el search

--- frontend/src/components/SearchPage.jsx
+++ frontend/src/components/SearchPage.jsx
@@ -1,167 +1,88 @@
-import React, { useState } from 'react'
+import React from 'react'
 import '../stylesheets/Search.css'
-import flechaIzq from '../images/flechaIzquierda.png';
-import flechaDer from '../images/flechaDerecha.png';
+import { Link } from 'react-router-dom'
 
 const objetoPrueba = [
     {
         nombre: "John",
         apellido: "Doe",
-        especialidad: "Boxeo",
-        descripcion: "Lorem ipsum dolor sit amet",
+        disciplina: "Boxeo",
         barrio: "Example",
         provincia: "Example",
-        valorSesion: "$ 2500",
-        disponibilidad: [["13:00 - 14:00"],["15:00 - 16:00"],["14:00 - 15:00"],["10:00 - 11:00"],["18:00 - 19:00"],["10:00 - 11:00"]],
     },
     {
         nombre: "Juan",
         apellido: "Perez",
-        especialidad: "Zumba",
-        descripcion: "Lorem ipsum dolor sit amet",
+        disciplina: "Zumba",
         barrio: "Villa Urquiza",
         provincia: "Villa Urquiza",
-        valorSesion: "$ 5000",
-        disponibilidad: [["15:00 - 16:00"], [""],["18:00 - 19:00"], ["10:00 - 11:00"],[""],[""]],
     },
     {
         nombre: "Daniel",
         apellido: "Garcia",
-        especialidad: "Spinning",
-        descripcion: "Lorem ipsum dolor sit amet",
+        disciplina: "Spinning",
         barrio: "Boedo",
         provincia: "Boedo",
-        valorSesion: "$ 3500",
-        disponibilidad: [["15:00 - 16:00"], ["18:00 - 19:00"], [""], ["10:00 - 11:00"],[""],["16:00 - 17:00"]],
     }
 ]
 
-const Calendario = ({ persona, handleAnteriorSemana, handleSiguienteSemana }) => {
-  const EncabezadoCalendario = ({ fechas }) => {
-    return (
-        <thead>
-            <tr>
-                {fechas.map((fecha, index) => (
-                    <th key={index}>
-                        <span style={{ display: 'block' }}>{fecha.split(' ')[1]}</span>
-                        <span style={{ display: 'block' }}>{fecha.split(' ')[0]}</span>
-                    </th>
-                ))}
-            </tr>
-        </thead>
-    );
-  };
-  
-  const obtenerFechasSemana = (fecha) => {
-      const diasSemana = ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"];
-      const fechas = [];
-      for (let i = 1; i <= 6; i++) { // Modificado para mostrar solo de Lunes a Sábado
-          const nuevaFecha = new Date(fecha.getTime() + i * 24 * 60 * 60 * 1000);
-          const diaNumero = nuevaFecha.getDate();
-          const mes = nuevaFecha.getMonth() + 1;
-          fechas.push(`${diaNumero < 10 ? '0' : ''}${diaNumero}/${mes < 10 ? '0' : ''}${mes} ${diasSemana[nuevaFecha.getDay()]}`);
-      }
-      return fechas;
-  };
-  
-  return (
-    <div className="calendario">
-      <div className="tabla-calendario">
-      <table>
-        <EncabezadoCalendario fechas={obtenerFechasSemana(new Date())} />
-        <tbody>
-          {[...Array(6)].map((_, indexFila) => (
-            <tr key={indexFila}>
-              {persona.disponibilidad.map((horarios, indexColumna) => (
-                <td key={indexColumna} data-horario={horarios[indexFila]}>
-                  {horarios[indexFila]}
-                </td>
-              ))}
-            </tr>
-          ))}
-        </tbody>
-      </table>  
-      </div>
-      <div className="navegacion-calendario">
-        <button className='mover' onClick={handleAnteriorSemana}><img src={flechaIzq} alt="" /> </button>
-        <button className='mover' onClick={handleSiguienteSemana}><img src={flechaDer} alt="" /> </button>
-      </div>
-    </div>
-  );
-};
-
 const SearchPage = () => {
-    const hoy = new Date(); // Fecha actual
-    const [mostrarCalendario, setMostrarCalendario] = useState(false);
-    const [fechaActual, setFechaActual] = useState(hoy);
-    const [idPersonaSeleccionada, setIdPersonaSeleccionada] = useState(null);
-
     const handleVer = (id) => {
-      setIdPersonaSeleccionada(id);
-      setMostrarCalendario(true);
-    };
-
-    const handleAnteriorSemana = () => {
-        const nuevaFecha = new Date(fechaActual);
-        nuevaFecha.setDate(nuevaFecha.getDate() - 7);
-        setFechaActual(nuevaFecha);
-    };
-    
-    const handleSiguienteSemana = () => {
-        const nuevaFecha = new Date(fechaActual);
-        nuevaFecha.setDate(nuevaFecha.getDate() + 7);
-        setFechaActual(nuevaFecha);
-    };
-
-    const handleReservar = () => {
-        console.log(idPersonaSeleccionada);
-    }
+      };
 
     return (
-      <div className="listado">
-        <h1 className="texto_rojo">Resultados búsqueda</h1>
-        <table className="lista_prof">
-          <thead>
-            <tr>
-              <th>Nombre</th>
-              <th>Apellido</th>
-              <th>Especialidad</th>
-              <th>Descripción</th>
-              <th>Barrio</th>
-              <th>Provincia/Estado</th>
-              <th>Valor Sesión</th>
-              <th>Reservar</th>
-            </tr>
-          </thead>
-          <tbody>
-            {objetoPrueba && objetoPrueba.map((persona, index) => (
-              <tr key={index} id={`persona-${index}`}>
-                <td>{persona.nombre}</td>
-                <td>{persona.apellido}</td>
-                <td>{persona.especialidad}</td>
-                <td>{persona.descripcion}</td>
-                <td>{persona.barrio}</td>
-                <td>{persona.provincia}</td>
-                <td>{persona.valorSesion}</td>
-                <td><button className="ver" onClick={() => handleVer(index)}>Ver</button></td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-        {mostrarCalendario && (
-          <>
-            <Calendario 
-              persona={objetoPrueba[idPersonaSeleccionada]}
-              handleAnteriorSemana={handleAnteriorSemana} 
-              handleSiguienteSemana={handleSiguienteSemana}
-            />
-            <div className="posicion2">  
-              <button className="reserva" onClick={handleReservar}>Reservar</button>
+      <div>
+        <div className="searchbar">
+          <h2 className="h2">Buscar</h2>
+          <div className='inputsBuscador'>
+            <div>  
+              <input type="text" name="barrio" placeholder="Barrio" />
             </div>
-          </>
-        )}
-      </div>
+            <div>
+              <input type="text" name="provincia" placeholder="Provincia/Estado" />
+            </div>
+            <div>
+              <select name="disciplina">
+                <option value="">Todas las disciplinas</option>
+                <option value="Boxeo">Boxeo</option>
+                <option value="Zumba">Zumba</option>
+                <option value="Crossfit">Crossfit</option>
+                <option value="Gap">Gap</option>
+                <option value="Pilates">Pilates</option>
+              </select>
+            </div>
+            <div>
+              <Link style={{textDecoration: 'none'}} to={'/search'}><input className='buscar hover' type="submit" name="buscar" value="Buscar" /></Link>
+            </div>
+          </div>
+        </div>
+        <div className="listado">
+          <h1 className="texto_rojo">Resultados búsqueda</h1>
+          <div className="lista_prof">
+            <div className="encabezado">
+              <div className="columna titulos-encab">Nombre</div>
+              <div className="columna titulos-encab">Apellido</div>
+              <div className="columna titulos-encab">Disciplina</div>
+              <div className="columna titulos-encab">Barrio</div>
+              <div className="columna titulos-encab">Provincia/Estado</div>
+              <div className="columna titulos-encab">Reservar</div>
+            </div>
+            {objetoPrueba.map((persona, index) => (
+              <div key={index} id={`persona-${index}`} className="fila-prof">
+                <div className="columna">{persona.nombre}</div>
+                <div className="columna">{persona.apellido}</div>
+                <div className="columna">{persona.disciplina}</div>
+                <div className="columna">{persona.barrio}</div>
+                <div className="columna">{persona.provincia}</div>
+                <div className="columna">
+                  <button className="ver" onClick={() => handleVer(index)}>Ver</button>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>  
     );
-  };
+};
 
 export default SearchPage;
