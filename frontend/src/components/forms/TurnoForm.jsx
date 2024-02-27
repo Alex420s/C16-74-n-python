f3a5ac029bb7dcb77652afee0595250eddcd98e0
Wius <wius93@gmail.com>
2024-02-26 23:33:35 -0300

    Termiar prototipo turnoForm

--- frontend/src/components/forms/TurnoForm.jsx
+++ frontend/src/components/forms/TurnoForm.jsx
@@ -27,7 +27,7 @@ const TurnoForm = () => {
     } catch (error) {
       console.error('Error:', error.response.data);
     }
-    }
+  }
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -35,36 +35,44 @@ const TurnoForm = () => {
     console.log(formData);
   }
 
-  /*return (
-    <> 
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor='date'>Fecha</label>
-        <input name='date' placeholder='Fecha' onChange={handleChange}/>
-      </div>
-      <div>
-        <label htmlFor='date'>Hora</label>
-        <input name='time' placeholder='Hora' onChange={handleChange} />
-      </div>
-      <div>
-        <label htmlFor='date'>Categoría</label>
-        <input name='category' placeholder='Categoría' onChange={handleChange} />
-      </div>
-      <div>
-        <label htmlFor='date'>Capacidad</label>
-        <input name='capacity' placeholder='Capacidad' onChange={handleChange} />
-      </div>
-      <div>
-        <label htmlFor='date'>Mensaje</label>
-        <textarea name='message' placeholder='Comentario' onChange={handleChange} />
-      </div>
-      <div>
-        <label htmlFor='date'>Repetir semanalmente</label>
-        <input name='repeat' type='checkbox' value={true} onChange={handleChange}/>
-      </div>
-    </form>
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor='date'>Fecha</label>
+          <input name='date' placeholder='Fecha' onChange={handleChange} />
+        </div>
+        <div>
+          <label htmlFor='date'>Hora</label>
+          <input name='time' placeholder='Hora' onChange={handleChange} />
+        </div>
+        <div>
+          <label htmlFor='date'>Categoría</label>
+          <input name='category' placeholder='Categoría' onChange={handleChange} />
+        </div>
+        <div>
+          <label htmlFor='date'>Capacidad</label>
+          <input name='capacity' placeholder='Capacidad' onChange={handleChange} />
+        </div>
+        <div>
+          <label htmlFor='date'>Mensaje</label>
+          <textarea name='message' placeholder='Comentario' onChange={handleChange} />
+        </div>
+        <div>
+          <label htmlFor='date'>Repetir semanalmente</label>
+          <input name='repeat' type='checkbox' value={true} onChange={handleChange} />
+        </div>
+        <div>
+        {/* input temporal */}
+          <label htmlFor='intructor_id'>instructor_id</label>
+          <input name='instructor_id' placeholder='Id del instructor' onChange={handleChange} />
+        </div>
+        <div>
+          <input type='submit' value='Crear' />
+        </div>
+      </form>
     </>
-  )*/
+  )
 }
 
 export default TurnoForm
