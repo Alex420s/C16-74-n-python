f9cc9fd177dee55e7db3f34f1e372af56d0feeb2
Marcial Abad <108768099+Marcialess@users.noreply.github.com>
2024-03-03 23:37:15 -0300

    estructurar en modulos codigo

--- backend_FastAPI/main.py
+++ backend_FastAPI/main.py
@@ -1,10 +1,5 @@
-from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
-from typing import List, Optional
-from datetime import datetime, time
-from fastapi.middleware.cors import CORSMiddleware
-import secrets
-# from enum import Enum
+from .utils import *
+from .models import *
 
 app = FastAPI()
 
@@ -29,104 +24,10 @@ def generate_dummy_token(user_id: int) -> str:
     return token
 
 
-# Definir los modelos de datos
-class UserBase(BaseModel):
-    email: str
-    nick_name: str
-    first_name: str
-    last_name: str
-    phone_number: str
-    address: str
-    role: str
-
-
-class UserCreate(UserBase):
-    password: str
-
-
-class UserUpdate(UserBase):
-    password: str = None
-
-
-class User(UserBase):
-    id: int
-    registration_date: str
-    is_active: bool
-    is_staff: bool
-
-
-class UserResponse(BaseModel):
-    id: int
-    token: str
-    first_name: str
-    role: str
-
-
-class ProfessionalCreate(UserCreate):
-    speciality: str = "Contador"
-    description: str = "Profesional de confianza"
-    availability_hours: str = ""
-    neighborhood: str = ""
-    province: str = "Buenos Aires"
-    image: str = "default.jpg"
-
-
-class Professional(User):
-    professional_id: int
-    speciality: str
-    description: str
-    availability_hours: str
-    neighborhood: str
-    province: str
-    image: str
-
-
 # Base de datos simulada
 database = []
 availabilities_db = []
 turns_db = []
-
-
-# Define los modelos de datos para los turnos y la disponibilidad
-class AvailabilityBase(BaseModel):
-    professional_id: int
-    day_of_week: str
-    start_time: time
-    end_time: time
-    status: bool
-
-
-class AvailabilityCreate(AvailabilityBase):
-    pass
-
-
-class Availability(AvailabilityBase):
-    availability_id: int
-
-    class Config:
-        orm_mode = True
-
-
-class TurnBase(BaseModel):
-    professional_id: int
-    availability_id: int
-    user_id: int
-    date_and_time_of_turn: datetime
-    start_time: time
-    end_time: time
-    turn_status: str
-    message_to_professional: Optional[str]
-
-
-class TurnCreate(TurnBase):
-    pass
-
-
-class Turn(TurnBase):
-    turn_id: int
-
-    class Config:
-        orm_mode = True
 
 
 # Operaciones CRUD para disponibilidades
---
+++ backend_FastAPI/utils.py
@@ -0,0 +1,8 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from typing import List, Optional
+from datetime import datetime, time
+from fastapi.middleware.cors import CORSMiddleware
+import secrets
+# from enum import Enum
+from models import *
