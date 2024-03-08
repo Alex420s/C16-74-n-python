a363769b1364a9aa09e76a90b9846b6c3a9f51f5
Wius <wius93@gmail.com>
2024-03-08 12:40:30 -0300

    Merge con rama flask

--- frontend/src/components/ProfEdit.jsx
+++ frontend/src/components/ProfEdit.jsx
@@ -1,172 +1,292 @@
-import React, { useState } from 'react'
-import '../stylesheets/ProfesionalEdit.css'
-import image from '../images/image.jpg'
-import { MdAddAPhoto } from "react-icons/md";
+import React, { useState, useEffect } from "react";
+import "../stylesheets/ProfesionalEdit.css";
+import image from "../images/image.jpg";
 import { FaPlus } from "react-icons/fa";
+import { ImCross } from "react-icons/im";
+import axios from "axios";
 
 const ProfEdit = () => {
-  // Formulario utilizado para editar datos a un usuario existente
-  // https://render-api-a6du.onrender.com/user/professional
+  const id = localStorage.getItem("id");
+
   const [formData, setFormData] = useState({
-    first_name: '',
-    last_name: '',
-    email: '',
-    speciality: '',
-    description: '',
-    city: '',
-    province: '',
-    cbu: '',
-    alias: '',
+    id: id,
+    first_name: "",
+    last_name: "",
+    email: "",
+    description: "",
+    city: "",
+    province: "",
+    cbu: "",
+    bank: "",
+    photo: null,
   });
 
   // Función para manejar cambios en los campos del formulario
-  const handleChange = (event) => {
-    setFormData({...formData, [event.target.name]: event.target.value});
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
     console.log(formData); // Temporal; solo para probar
   };
 
-  // Función para manejar cambios en la imagen
   const handleImageUpload = (e) => {
     const file = e.target.files[0];
-    const reader = new FileReader();
+    setFormData({
+      ...formData,
+      photo: file,
+    });
+  };
 
-    // Agregar campo de "foto" al objeto `formData`
-    reader.onloadend = () => {
-      setFormData({...formData, image: reader.result}); // Reemplaza la imagen por defecto con la subida
-    };
-
-    if (file) {
-      reader.readAsDataURL(file);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formDataToSend = new FormData();
+    Object.entries(formData).forEach(([key, value]) => {
+      formDataToSend.append(key, value);
+    });
+    console.log(formDataToSend);
+    try {
+      const response = await axios.post(
+        "https://c16-74-n-python.onrender.com/editar-profesional/" + id,
+        formDataToSend
+      );
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
     }
   };
 
+  const [rows, setRows] = useState([]);
 
-  // Esta variable se debe llenar con los turnos registrados en la base de datos utilizando un useEffect
-  const [additionalRows, setAdditionalRows] = useState([
-    {
-      day: 'martes',
-      date: '05-03-2024',
-      time: '15:00',
-      price: '5000',
-      capacity: '15',
-    },
-  ]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          `https://c16-74-n-python.onrender.com/turnos-profesional/${id}`
+        );
+        setRows(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, [id]);
 
   // Variable para agregar turnos
   const [newRow, setNewRow] = useState({
-    day: '',
-    date: '',
-    time: '',
-    price: '',
-    capacity: '',
+    category: "",
+    date: "",
+    time: "",
+    address: "",
+    cost: "",
+    capacity: "",
   });
 
   // Función para agregar turnos
-  const addRow = () => {
-    setAdditionalRows((prevState) => [...prevState, { ...newRow }]);
-    setNewRow({ day: '', date: '', time: '', price: '', capacity: '' }); // Limpia el formulario
+  const addRow = async () => {
+    const response = await axios.post(
+      "https://c16-74-n-python.onrender.com/crear-turno/" + id,
+      newRow
+    );
+    console.log(response.data);
+    setRows((prevState) => [...prevState, { ...newRow }]);
+    setNewRow({
+      category: "",
+      date: "",
+      time: "",
+      address: "",
+      cost: "",
+      capacity: "",
+    }); // Limpia el formulario
   };
 
   // Función para manejar cambios en los turnos
   const handleRowChange = (event) => {
     setNewRow({ ...newRow, [event.target.name]: event.target.value });
-    console.log(newRow); // Temporal; solo para probar
-  }
+  };
 
   return (
     <div className="todo">
-      <div className="cuerpoEdit">
-        <div>
-          <div className="info-1">
-            <p className='tituloInfo'>Información personal</p>
-            <div>
-              <div className="imagen-subir">
-<<<<<<< HEAD
-                <img className="fotoSubir" src={formData.image} alt="Imagen de perfil" />
-                <input
-                  id="file-upload"
-                  type="file"
-                  className="imagen-subir-input"
-                  onChange={handleImageUpload}
-                  accept="image/*"
-                />
-=======
-                <img className="fotoSubir" src={formData.image || require('../images/image.jpg')} alt="Imagen de perfil" />
-                <input id="file-upload" type="file" className="imagen-subir-input" onChange={handleImageUpload} accept="image/*" />
->>>>>>> 397a0b624e41ebb0d0dcbd74ffbf458a7309b888
+      <form onSubmit={handleSubmit}>
+        <div className="cuerpoEdit">
+          <div>
+            <div className="info-1">
+              <p className="tituloInfo">Información personal</p>
+              <div>
+                <div className="imagen-subir">
+                  <img
+                    className="fotoSubir"
+                    src={
+                      formData.photo
+                        ? URL.createObjectURL(formData.photo)
+                        : image
+                    }
+                    alt="Imagen de perfil"
+                  />
+                  <input
+                    id="file-upload"
+                    type="file"
+                    className="imagen-subir-input"
+                    onChange={handleImageUpload}
+                    accept="image/*"
+                  />
+                </div>
+                <div className="fila">
+                  <input
+                    className="editP"
+                    type="text"
+                    placeholder="Nombre"
+                    required
+                    name="first_name"
+                    value={formData.first_name}
+                    onChange={handleChange}
+                  />
+                  <input
+                    className="editP"
+                    type="text"
+                    placeholder="Apellido"
+                    required
+                    name="last_name"
+                    value={formData.last_name}
+                    onChange={handleChange}
+                  />
+                </div>
+                <div className="fila">
+                  <input
+                    className="editP"
+                    type="email"
+                    name="email"
+                    id="email"
+                    required
+                    placeholder="Email"
+                    value={formData.email}
+                    onChange={handleChange}
+                  />
+                </div>
+                <div className="fila">
+                  <input
+                    className="editP"
+                    type="number"
+                    placeholder="CBU"
+                    required
+                    name="cbu"
+                    value={formData.cbu}
+                    onChange={handleChange}
+                  />
+                  <input
+                    className="editP"
+                    type="text"
+                    placeholder="Banco"
+                    required
+                    name="bank"
+                    value={formData.alias}
+                    onChange={handleChange}
+                  />
+                </div>
               </div>
-              <div className="fila">
-                <input className="editP" type="text" placeholder="Nombre" required name="first_name" value={formData.first_name} onChange={handleChange} />
-                <input className="editP" type="text" placeholder="Apellido" required name="last_name" value={formData.last_name} onChange={handleChange} />
-              </div>
-              <div className="fila">
-                <input className="editP" type="email" name="email" id="email" required placeholder="Email" value={formData.email} onChange={handleChange} />
-              </div>
-              <div className="fila">
-                <input className="editP" type="number" placeholder="CBU" required name="CBU" value={formData.cbu} onChange={handleChange} />
-                <input className="editP" type="text" placeholder="Alias cta" required name="Alias_transferencia" value={formData.alias} onChange={handleChange} />
+            </div>
+          </div>
+          <div>
+            <div className="info-2">
+              <p className="tituloInfo">Informacion profesional</p>
+              <div>
+                <textarea
+                  name="description"
+                  id="descripcion"
+                  placeholder="Descripción"
+                  value={formData.description}
+                  onChange={handleChange}
+                ></textarea>
               </div>
             </div>
           </div>
         </div>
-        <div>
-          <div className="info-2">
-            <p className='tituloInfo'>Informacion profesional</p>
-            <div>
-              <select name="speciality" id="discipline" value={formData.speciality} onChange={handleChange}>
-                <option value="Boxeo">Boxeo</option>
-                <option value="Zumba">Zumba</option>
-                <option value="Crossfit">Crossfit</option>
-                <option value="Gap">Gap</option>
-                <option value="Pilates">Pilates</option>
-                <option value="Yoga">Yoga</option>
-              </select>
-              <textarea name="description" id="descripcion" placeholder="Descripción" value={formData.description} onChange={handleChange}></textarea>
-            </div>
-          </div>
-          <div className="info-3">
-            <p className='tituloInfo'>Información clases</p>
-            <div className="fila">
-              <input className="editP" type="text" name="city" id="Barrio" required placeholder="Barrio" value={formData.city} onChange={handleChange} />
-              <input className="editP" type="text" name="province" id="Provincia" required placeholder="Provincia/Estado" value={formData.province} onChange={handleChange} />
-            </div>
-          </div>
+        <div className="contenedor-guardar">
+          <input
+            className="guardar hover"
+            type="submit"
+            name="guardar"
+            value="Guardar"
+          />
         </div>
-      </div>
-      <div className="contenedor-guardar">
-        <input className="guardar hover" type="submit" name="guardar" value="Guardar" />
-      </div>
-      <p className='h2NvosTurnos'>Agendá nuevos turnos</p>
-      {additionalRows.map((row, index) => (
-          <div className='turnoAgendar' key={index}>
-            <input className="editP2" readOnly={true} value={row.day}/>
-            <input className="editP2" readOnly={true} value={row.date} />
-            <input className="editP2" readOnly={true} value={row.time} />
-            <input className="editP2" readOnly={true} value={row.price} />
-            <input className="editP2" readOnly={true} value={row.capacity} />
-          </div>
-        ))}
+      </form>
+      <p className="h2NvosTurnos">Agendá nuevos turnos</p>
+      {rows.map((row, index) => (
+        <div className="turnoAgendar" key={index}>
+          <input className="editP2" readOnly={true} value={row.category} />
+          <input className="editP2" readOnly={true} value={row.date} />
+          <input className="editP2" readOnly={true} value={row.time} />
+          <input className="editP2" readOnly={true} value={row.address} />
+          <input className="editP2" readOnly={true} value={row.cost} />
+          <input className="editP2" readOnly={true} value={row.capacity} />
+          <span>
+            {" "}
+            <ImCross />
+          </span>
+        </div>
+      ))}
       <div className="turnoAgendar">
-        <select name="day" id="dia" value={newRow.day} onChange={handleRowChange} className='editP2'>
-          <option value="lunes">Lunes</option>
-          <option value="martes">Martes</option>
-          <option value="miercoles">Miércoles</option>
-          <option value="jueves">Jueves</option>
-          <option value="viernes">Viernes</option>
-          <option value="sabado">Sábado</option>
+        <select className="editP2" name="category" onChange={handleRowChange}>
+          <option value="">Todas las disciplinas</option>
+          <option value="Boxeo">Boxeo</option>
+          <option value="Zumba">Zumba</option>
+          <option value="Crossfit">Crossfit</option>
+          <option value="Gap">Gap</option>
+          <option value="Pilates">Pilates</option>
         </select>
-        <input className="editP2" type="date" name="date" required value={newRow.date} onChange={handleRowChange} />
-        <input className="editP2" type="time" name="time" required value={newRow.time} onChange={handleRowChange} />
-        <input className="editP2" type="number" min="0" name="price" required placeholder="Precio" value={newRow.price} onChange={handleRowChange} />
-        <input className="editP2" type="number" min="0" name="capacity" required placeholder='Cupo' value={newRow.capacity} onChange={handleRowChange} />
+        <input
+          className="editP2"
+          type="date"
+          name="date"
+          required
+          value={newRow.date}
+          onChange={handleRowChange}
+        />
+        <input
+          className="editP2"
+          type="time"
+          name="time"
+          required
+          value={newRow.time}
+          onChange={handleRowChange}
+        />
+        <input
+          className="editP2"
+          type="text"
+          name="address"
+          required
+          placeholder="Dirección"
+          value={newRow.address}
+          onChange={handleRowChange}
+        />
+        <input
+          className="editP2"
+          type="number"
+          min="0"
+          name="cost"
+          required
+          placeholder="Precio"
+          value={newRow.cost}
+          onChange={handleRowChange}
+        />
+        <input
+          className="editP2"
+          type="number"
+          min="0"
+          name="capacity"
+          required
+          placeholder="Cupo"
+          value={newRow.capacity}
+          onChange={handleRowChange}
+        />
         <div className="mas hover">
-          <span onClick={addRow}> <FaPlus /></span>
+          <span onClick={addRow}>
+            {" "}
+            <FaPlus />
+          </span>
         </div>
         <br />
-        
       </div>
     </div>
   );
-}
+};
 
-export default ProfEdit
+export default ProfEdit;
